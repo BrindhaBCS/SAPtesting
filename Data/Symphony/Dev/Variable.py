677fec613f7b28e13cb087b5e261c144928a4dd0
BrindhaBCS <128508368+BrindhaBCS@users.noreply.github.com>
2024-02-23 07:38:12 +0000

    Update Variable.py

--- Data/Symphony/Dev/Variable.py
+++ Data/Symphony/Dev/Variable.py
@@ -1,5 +1,5 @@
 EXE_PAD = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
-Connection = "RBT"
+Connection_Name = "RBT"
 SAP_CLIENT = "000"
 SAP_USER = "DDIC"
 addOn = ["BNWVS",    "ST-A/PI"]    
