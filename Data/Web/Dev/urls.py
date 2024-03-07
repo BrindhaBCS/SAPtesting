6a14e3252f245a2f27766cd556999d82fbc06125
khadhijah <mailtokhadhijah@gmail.com>
2024-03-07 17:05:58 +0530

    main brach

--- Data/Web/Dev/urls.py
+++
@@ -1,5 +0,0 @@
-# Tracks url data variable values. Create separate folders in future if this grows.
-
-#url = "https://dev.rise.jnj.com/"
-# url ="https://qa.symphony4sap.com/login"
-url ="https://predev.symphony4sap.com/login"
