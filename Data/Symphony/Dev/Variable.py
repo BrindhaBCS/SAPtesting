d5c62a3b73f8e23638d027d13874a6ba192062dd
Brindha <brindha.m@basiscloudsolutions.com>
2024-03-16 06:47:03 +0000

    global variable testing

--- Data/Symphony/Dev/Variable.py
+++ Data/Symphony/Dev/Variable.py
@@ -1,9 +1,5 @@
-EXE_PAD = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
-Connection_Name = "RBT"
-SAP_CLIENT = "000"
-SAP_USER = "DDIC"
-addOn = ["BNWVS",    "ST-A/PI"]    
-Patch = ["SAPK-70001INBNWVS",    "SAPKITABC5"]
-patch_comp = "ST-PI"
-patch_vers = "SAPK-74022INSTPI"
-SAP_PASSWORD = "Sym@rocks2023"
+SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPGUI\\saplogon.exe"
+Client_Id = "001"
+User_Name = "SELENIUM"
+User_Password = "Test@12345"
+SAP_connection = "BIS"
