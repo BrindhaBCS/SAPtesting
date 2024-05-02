c6a08e850abdd9e85e00da83bb0d2d520a227f82
laksh <lakshamanaperumal.n@basiscloudsolutions.com>
2024-05-02 16:28:33 +0530

    comment Force tags

--- Data/Symphony/Dev/Variable_BIS.py
+++ Data/Symphony/Dev/Variable_BIS.py
@@ -1,5 +1,4 @@
-SAP_SERVER = "C:\\Program Files\\SAP\\FrontEnd\\SAPGUI\\saplogon.exe"
+SAP_SERVER = "C:\\Program Files\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
 Client_Id = "001"
 User_Name = "SELENIUM"
-User_Password = "Test@12345"
 SAP_connection = "BIS"
