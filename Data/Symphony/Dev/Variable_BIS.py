4974d0c8fe89dbc7765817292cb3da093355a1fe
Brindha <brindha.m@basiscloudsolutions.com>
2024-08-05 05:40:19 +0000

    add

--- Data/Symphony/Dev/Variable_BIS.py
+++ Data/Symphony/Dev/Variable_BIS.py
@@ -3,6 +3,7 @@ Client_Id = "$Client_Id"
 User_Name = "$User_Name"
 SAP_connection = "$SAP_connection"
 
+
 # SAP_SERVER = "C:\\Program Files\\SAP\\FrontEnd\\SAPGUI\\saplogon.exe"
 # Client_Id = "001"
 # User_Name = "SELENIUM"
