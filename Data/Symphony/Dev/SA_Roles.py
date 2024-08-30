d8ecb32f58377f54b0640b74606f7dbd6b02f7ed
surya <suryaprakash.r@basiscloudsolutions.com>
2024-08-30 19:35:42 +0530

    upload_create_and_change_date_files

--- Data/Symphony/Dev/SA_Roles.py
+++ Data/Symphony/Dev/SA_Roles.py
@@ -17,9 +17,13 @@
 # SA_Role_User_Name = "Preethi"
 # SA_Role_User_Password = "Mylife@12345"
 # #test_user_crentials
+# Create_From_Date = "01.08.2024"
+# Create_To_Date ="30.08.2024"
 # Create_Date_User_Name = "TEST_ROLE"
 # Create_Date_User_Password = "Welcome@@123"
 # #su01_new_user create
+# Change_From_Date = "01.08.2024"
+# Change_To_Date = "30.08.2024"
 # Change_Date_User_Name = "Common_usr"
 # New_User_Password = "Welcome@123"
 # New_User_Last_Name = "check one by one"
