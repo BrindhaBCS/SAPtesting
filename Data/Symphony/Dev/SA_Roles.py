397ef97e6049ae4dd4b0b8c0c5fe85f63882de67
surya <suryaprakash.r@basiscloudsolutions.com>
2024-09-05 15:25:54 +0530

    new_enhancement

--- Data/Symphony/Dev/SA_Roles.py
+++ Data/Symphony/Dev/SA_Roles.py
@@ -9,23 +9,23 @@
 # Roles = ["/BCS/SYMPHONY_AWS","/BCS/SYMPHONY_DISP","/BCS/SYMPHONY_GCP"]
 # Execute = "wnd[0]/tbar[1]/btn[8]"
 
-#Own_login
-SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
-SA_Role_Connection = "TS4"
-SA_Role_Client_Id = "001"
-SA_Role_User_Name = "Preethi"
-SA_Role_User_Password = "Mylife@12345"
-#Create
-Create_From_Date = "20.08.2024"
-Create_To_Date ="30.08.2024"
-#Cahnge
-Change_From_Date = "20.08.2024"
-Change_To_Date = "30.08.2024"
-#su01_new_user create
-New_User_Name = "Sample_Test"
-New_User_Last_Name = "check one by one"
-New_User_Password = "Welcome@@@001"
-User_Reset_Current_Password = "Welcome@123"
+# #Own_login
+# SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
+# SA_Role_Connection = "TS4"
+# SA_Role_Client_Id = "001"
+# SA_Role_User_Name = "Preethi"
+# SA_Role_User_Password = "Mylife@12345"
+# #Create
+# Create_From_Date = "20.08.2024"
+# Create_To_Date ="30.08.2024"
+# #Cahnge
+# Change_From_Date = "20.08.2024"
+# Change_To_Date = "30.08.2024"
+# #su01_new_user create
+# New_User_Name = "Sample_Test"
+# New_User_Last_Name = "check one by one"
+# New_User_Password = "Welcome@@@001"
+# User_Reset_Current_Password = "Welcome@123"
 
 #Own_login
 SAP_SERVER = "$SAP_SERVER"
