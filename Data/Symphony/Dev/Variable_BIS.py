9e476a8a098acc629c2fdc23aca57c3ba1a8a2d0
laksh <lakshamanaperumal.n@basiscloudsolutions.com>
2024-09-12 17:19:37 +0530

    add

--- Data/Symphony/Dev/Variable_BIS.py
+++ Data/Symphony/Dev/Variable_BIS.py
@@ -1,7 +1,7 @@
-# SAP_SERVER = "$SAP_SERVER"
-# Client_Id = "$Client_Id"
-# User_Name = "$User_Name"
-# SAP_connection = "$SAP_connection"
+SAP_SERVER = "$SAP_SERVER"
+Client_Id = "$Client_Id"
+User_Name = "$User_Name"
+SAP_connection = "$SAP_connection"
 symphony_job_id ="$SymphonyJobId"
 
 
