6e888aa4856c071444bef81a9f46f82a30a9b093
Brindha <brindha.m@basiscloudsolutions.com>
2024-12-12 14:30:24 +0000

    Variable file updated

--- Data/Symphony/Dev/abinbev.py
+++ Data/Symphony/Dev/abinbev.py
@@ -1,5 +1,11 @@
-SAP_SERVER  = "C:\\Program Files (x86)\\SAP\FrontEnd\\SAPgui\\saplogon.exe"
-abhinbevSID = "S4hana"
-clientno = "100"
-diaUsername = "devuser2"
-diaUserpassword = "Bcshana@123"
+# SAP_SERVER  = "C:\\Program Files (x86)\\SAP\FrontEnd\\SAPgui\\saplogon.exe"
+# abhinbevSID = "S4hana"
+# clientno = "100"
+# diaUsername = "devuser2"
+
+
+abinbev_SAP_SERVER  = "$abinbev_SAP_SERVER"
+abhinbev_SID = "$abhinbev_SID"
+abinbev_clientno = "$abinbev_clientno"
+abinbev_diaUsername = "$abinbev_diaUsername"
+
