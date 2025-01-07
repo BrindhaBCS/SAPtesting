50a581c2736325d522ca3c3d1344b02ba5c22fc1
BrindhaBCS <brindha.m@basiscloudsolutions.com>
2025-01-07 07:23:44 +0000

    pull request

--- Data/Symphony/Dev/rental.py
+++ Data/Symphony/Dev/rental.py
@@ -4,4 +4,4 @@ Rental_Client = "001"
 Rental_User = "naren"
 Invoice_PDF_PATH = "C:\\Symphony\\Rental_Invoice\\Download"
 Layout = ["Contracts - Header",]
-documents = "40000063"
+documents = "40000065"
