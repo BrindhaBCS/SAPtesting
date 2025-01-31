783a13e98efe80e69686d01ccb3da190eb52c343
Brindha <brindha.m@basiscloudsolutions.com>
2025-01-31 05:44:19 +0000

    changes template

---
+++ Data/Symphony/Dev/Rental.py
@@ -0,0 +1,9 @@
+SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
+Rental_Connection = "TS4"
+Rental_Client = "001"
+Rental_User = "EIDTEST"
+RENTAL_PASSWORD ="Admin@1234"
+Invoice_PDF_PATH = "C:\\Symphony\\Rental_Invoice\\Download"
+Layout = ["Contracts - Header",]
+documents = "40000071"
+month_json = [{"Month":"January","Year":"2025"}]
--- Data/Symphony/Dev/Variable.py
+++ Data/Symphony/Dev/Variable.py
@@ -9,15 +9,7 @@
 # Connection_Name = "RBT"
 # SAP_CLIENT = "000"
 # SAP_USER = "DDIC"
-SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
-Rental_Connection = "TS4"
-Rental_Client = "001"
-Rental_User = "EIDTEST"
-RENTAL_PASSWORD ="Admin@1234"
-Invoice_PDF_PATH = "C:\\Symphony\\Rental_Invoice\\Download"
-Layout = ["Contracts - Header",]
-documents = "40000071"
-month_json = [{"Month":"January","Year":"2025"}]
+
 
 
 
