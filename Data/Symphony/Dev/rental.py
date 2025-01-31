f219816cdc8d9ce6a574da4c08954bad594ec1cb
BrindhaBCS <brindha.m@basiscloudsolutions.com>
2025-01-31 06:56:00 +0000

    send email with pdf

--- Data/Symphony/Dev/rental.py
+++ Data/Symphony/Dev/rental.py
@@ -4,5 +4,5 @@ Rental_Client = "001"
 Rental_User = "EIDTEST"
 Layout = ["Contracts - Header",]
 month_json = [{"Month":"January","Year":"2025"}]
-documents = "40000098"
+documents = "40000099"
 search_terms = ["Sold-To Party Name"]
