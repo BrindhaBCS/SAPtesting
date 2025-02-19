9606d147834b433afb14a9b8635d470c153efa0a
BrindhaBCS <brindha.m@basiscloudsolutions.com>
2025-02-19 10:41:03 +0000

    second scenario

--- Data/Symphony/Dev/rental.py
+++ Data/Symphony/Dev/rental.py
@@ -2,6 +2,6 @@ SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
 Rental_Connection = "TS4"
 Rental_Client = "001"
 Rental_User = "EIDTEST"
-month_json = [{"Month":"February","Year":"2025"}]
+month_json = [{"Month":"January","Year":"2025"}]
 Layout = ["Contracts - Header",]
 search_terms = ["Sold-To Party Name","Billing Block Description"]
