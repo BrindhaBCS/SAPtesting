2962e90cf7a6947e8fa01539898200d3e3cba70b
Suryaprakash-bcs <suryaprakash.r@basiscloudsolutions.com>
2025-02-24 17:47:11 +0530

    changed py file

--- Data/Symphony/Dev/Olympus.py
+++ Data/Symphony/Dev/Olympus.py
@@ -3,4 +3,7 @@ Olympus_SAP_connection = "SP1"
 Olympus_Client_Id = "001"
 Olympus_User_Name = "ddic"
 
- 
+ABB_Olympus_SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
+ABB_Olympus_SAP_connection = "ADT"
+ABB_Olympus_Client_Id = "000"
+ABB_Olympus_User_Name = "Sap*"
