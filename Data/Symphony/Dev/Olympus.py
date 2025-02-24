0ddbc923ecb39553d2eedfde58f77b635139ed60
Suryaprakash-bcs <suryaprakash.r@basiscloudsolutions.com>
2025-02-24 17:58:07 +0530

    new changes

--- Data/Symphony/Dev/Olympus.py
+++ Data/Symphony/Dev/Olympus.py
@@ -1,9 +1,20 @@
 Olympus_SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
-Olympus_SAP_connection = "SP1"
-Olympus_Client_Id = "001"
-Olympus_User_Name = "ddic"
+Olympus_SAP_connection = "$Olympus_SAP_connection"
+Olympus_Client_Id = "$Olympus_Client_Id"
+Olympus_User_Name = "$Olympus_User_Name"
 
 ABB_Olympus_SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
-ABB_Olympus_SAP_connection = "ADT"
-ABB_Olympus_Client_Id = "000"
-ABB_Olympus_User_Name = "Sap*"
+ABB_Olympus_SAP_connection = "$ABB_Olympus_SAP_connection"
+ABB_Olympus_Client_Id = "$ABB_Olympus_Client_Id"
+ABB_Olympus_User_Name = "$ABB_Olympus_User_Name*"
+
+
+# Olympus_SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
+# Olympus_SAP_connection = "SP1"
+# Olympus_Client_Id = "001"
+# Olympus_User_Name = "ddic"
+
+# ABB_Olympus_SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
+# ABB_Olympus_SAP_connection = "ADT"
+# ABB_Olympus_Client_Id = "000"
+# ABB_Olympus_User_Name = "Sap*"
